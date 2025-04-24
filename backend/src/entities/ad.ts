83449360125b00a7a356babe0bf13eaaa5133a00
Clément <clement.thorez1@gmail.com>
2025-04-24 10:22:00 +0200

    installation graphQL et modification entities Ad

--- backend/src/entities/ad.ts
+++ backend/src/entities/ad.ts
@@ -10,31 +10,40 @@ import {
 } from "typeorm";
 import Category from "./category";
 import Tag from "./tag";
+import { Field, ObjectType } from "type-graphql";
 
 @Entity()
+@ObjectType()
 class Ad extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
+  @Field()
   title: string;
 
   @Column()
+  @Field()
   description: string;
 
   @Column()
+  @Field()
   owner: string;
 
   @Column()
+  @Field()
   price: number;
 
   @Column()
+  @Field()
   picture: string;
 
   @CreateDateColumn()
+  @Field()
   createdAt: Date;
 
   @Column()
+  @Field()
   city: string;
 
   @ManyToOne(() => Category, (category) => category.ads)
